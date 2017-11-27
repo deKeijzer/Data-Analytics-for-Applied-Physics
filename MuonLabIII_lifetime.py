aa4e9b345e5642bbef415986aa7a3e7207a7f6da
Brian de Keijzer <briandekeyzer@hotmail.com>
2017-11-27 21:48:13 +0100

    cleaned up code

--- MuonLabIII_lifetime.py
+++ MuonLabIII_lifetime.py
@@ -4,7 +4,6 @@ import Data_analytics_TN as da
 import os
 import scipy as sp
 import matplotlib.pyplot as plt
-from matplotlib import rc
 import locale
 
 global df
@@ -14,6 +13,7 @@ plt.rcdefaults()
 locale.setlocale(locale.LC_NUMERIC, "dutch")
 # Tell matplotlib to use the locale we set above
 plt.rcParams['axes.formatter.use_locale'] = True
+
 
 def create_df(directory, file_name, i, file_format):
     """
@@ -28,9 +28,6 @@ def create_df(directory, file_name, i, file_format):
     path = str(directory) + str(file_name) + str(i) + str(file_format)
     if os.path.isfile(path):
         df = pd.read_csv(path, sep='\t', header=1, names=['lifetimes', 'counts'], decimal=".")
-        # ---- START OF DATAFRAME MANIPULATIONS ----
-        #df_TN['time'] = add_index_to_time(50)  # Adds ['time'] column to df_TN
-        #df_TN['time'] = add_index_to_time(1)
     else:
         print(path+' does not excist')
 
@@ -45,7 +42,7 @@ df_edited = pd.DataFrame()
 sample_number = ''
 create_df('Data\\MuonLab\\2\\', 'lifetime', sample_number, '.txt')
 # Remove 0's from df because those arn't measures values from MuonLabIII
-df = df[~(df == 0).any(axis=1)]  # Verpest de fit
+df = df[~(df == 0).any(axis=1)]
 da.df_TN = df
 
 da.data_hist_to_raw('lifetimes', 'counts')
@@ -53,7 +50,6 @@ df = da.df_TN_hist_to_raw
 df = df.sort_values('lifetimes', ascending=False)  # Sort df['lifetimes'] from high to low value
 df = df.reset_index(drop=True)  # Reset index numbers back to default (0...100) instead of (100...0)
 
-#sample = df['z'].iloc[min_index_val:max_index_val]  # The sample range to do statistical analysis on
 sample = df['lifetimes']
 da.sample = sample
 
@@ -64,24 +60,8 @@ End of dataframe settings
 """
 -----START OF USER VARIABLES-----
 """
-# Label data
 da.y_label = 'Versnelling'
 da.x_label = 'Tijd (s)'
-
-
-
-
-# Edit the df_live to df_edited using data_hist_to_raw()
-
-
-#df = df_edited  # Specify which dataframe to use for calculations & plots
-
-
-#df['time'] = df['time']*10E-6  # Significance corrections
-
-
-
-# print(df['time'])
 
 da.mu = 2.19704  # literature value to compare the sample to
 da.x_significant_digits = 3  # Number of significant digits used in plots
@@ -91,8 +71,8 @@ da.y_significant_digits = 3  # Number of significant digits used in plots
 -----END OF USER VARIABLES-----
 """
 
-da.x_bar = np.mean(sample) #x bar, sample average
-da.s = np.std(sample) #sigma, sample standard deviation
+da.x_bar = np.mean(sample) # x bar, sample average
+da.s = np.std(sample) # sigma, sample standard deviation
 da.delta_x = da.s/(len(sample))**(1/2) #d
 da.mode = sp.stats.mode(sample, axis=None)
 
@@ -101,33 +81,13 @@ print('s: %s'% da.s)
 print('delta_x: %s'% da.delta_x)
 da.t_test(da.sample, da.mu)
 
-
-#print(stats.norm.ppf(1.96))
-#print(stats.norm.ppf(1.96))
-
-
+# data, number of bars, Enable gauss fit, Enable_axis_lim, x_lim, y_lim
 da.histogram(sample, 100, False, False, [-1.02, -1.0055], [0, 250],
-          r'$ \mathrm{Sample} \: 2: \quad \overline{\tau}_0 = %s \quad \Delta \overline{\tau}_0 = %s $'
+             r'$ \mathrm{Sample} \: 2: \quad \overline{\tau}_0 = %s \quad \Delta \overline{\tau}_0 = %s $'
              % (da.dot_to_comma(da.x_bar, 3), da.dot_to_comma(da.delta_x, 3)),
-             #% (da.x_bar, da.delta_x),
-          r'$ \mathrm{Levensduur} \: \tau_0 \: [\mu s] $',
-          r'$ \mathrm{Frequentie} \: n \: [-] $') # data, number of bars, Enable gauss fit, Enable_axis_lim, x_lim, y_lim
+             r'$ \mathrm{Levensduur} \: \tau_0 \: [\mu s] $',
+             r'$ \mathrm{Frequentie} \: n \: [-] $')
 
-#create_plot(df['U'], df['delta U'], df['mgd'], df['delta mgd'])
-#create_plot_without_error(df['t'], df['pressure'], sample_number, 'x', 'y')
-#linear_regression_plot(df['U'], df['delta U'], df['mgd'], df['delta mgd'])
-#da.logarithmic_fit_plot(df['lifetimes'], df['counts'])  # df.index.values gives index numbers as an array
-#line_fit_plot(df['time'], df['z'])
-#df_to_csv('raw.scv') #saves df to scv file 'file name'
 
-#acc_line_fit_plot(df['time'], df['z'], sample_number, r'$\mathrm{Tijd \:} (s)$',
-#                  r'$ \mathrm{Versnelling \: in \:} g \: (ms^-2)}$')
-
-#multi_plot_sense_hat()
-print(df)
 plt.show()
-
-#print(numeric_integration(df['time'], df['z']))
-
-#save_multiple_plots('Data\\Accelerometer (ACC)\\', 'acc_', '.csv', 1, 21)
 
