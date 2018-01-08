b2f09278832cc2bd9947bb6553a2f0aaa84c98b8
Brian de Keijzer <briandekeyzer@hotmail.com>
2018-01-08 22:25:50 +0100

    .

--- Data/VO/Meting gewichten/resultaat.m
+++ Data/VO/Meting gewichten/resultaat.m
@@ -9,12 +9,12 @@ spanning_piek1 = sample(:,3);
 freq_piek2 = sample(:,4);
 spanning_piek2 = sample(:,5);
 
-fit_piek2(massa, spanning_piek2);
+fit_piek1(massa, spanning_piek1);
 
 
 % labels & legend zelf aanpassen in createFit
 
-legend('Meetwaarden', 'Fit: ax+b      a=8,251\times10^{-5}     b=2,131\times10^{-6}     R^2=0.8161', 'Interpreter', 'latex', 'Location' ,'NorthWest')
+legend('Meetwaarden', 'Fit: ax+b      a=13,57\times10^{-5}     b=9,412\times10^{-6}     R^2=0,8820', 'Interpreter', 'latex', 'Location' ,'NorthWest')
 
 % Correcte significantie maken voor plot 1
 xtickformat('%.3f')
