9c17335138dc1d44e123b01f2910ac57d2180201
Brian de Keijzer <briandekeyzer@hotmail.com>
2018-01-11 12:23:12 +0100

    .

--- Data/VO/Meting gewichten/gevoeligheid.m
+++ Data/VO/Meting gewichten/gevoeligheid.m
@@ -4,7 +4,7 @@ time_raw = sample(:,5);
 
 % meting = sample(:,1);
 massa = sample(:,1);
-freq_piek1 = sample(:,2);
+% freq_piek1 = sample(:,2);
 % spanning_piek1 = sample(:,3);
 freq_piek2 = sample(:,4);
 spanning_piek2 = sample(:,5);
@@ -16,7 +16,7 @@ r = 4.3E-2; % 5.9 +- 0.5 + 1 cm? voor radius vd pijp waar die aan zit
 m_tot = massa + rust_massa;
 
 % kracht = omega.^2*r.*m_tot;
-a_z = r*omega.^2
+a_z = r*omega.^2;
 % kracht = m_tot.*(a_z)
 
 
@@ -26,24 +26,25 @@ fit_gevoeligheid_piek1(kracht, spanning_piek1)
 
 % labels & legend zelf aanpassen in createFit
 xlim([0 14])
-legend('Meetwaarden', 'Fit: ax+b      a=9,778\times10^{-6}     b=-9,980\times10^{-7}     R^2=0,9451', 'Interpreter', 'latex', 'Location' ,'NorthWest')
-% legend('Meetwaarden', 'Fit', 'Interpreter', 'latex', 'Location' ,'NorthWest')
+ylim([0 1.5E-4])
+% legend('Meetwaarden', 'Fit: ax+b      a=9,796\times10^{-6}     b=-1,169\times10^{-6}     R^2=0,7433', 'Interpreter', 'latex', 'Location' ,'NorthWest')
+legend('Meetwaarden', 'Fit', 'Interpreter', 'latex', 'Location' ,'NorthWest')
 
 % Correcte significantie maken voor plot 1
 xtickformat('%.3f')
 ytickformat('%.3f')
 
-% Punt naar comma veranderen voor de de assen van plot 1
-x = get(gca, 'XTickLabel');
-nieuw_x = strrep(x(:),'.',',');
-set(gca, 'XTickLabel', nieuw_x)
-y = get(gca, 'YTickLabel');
-nieuw_y = strrep(y(:),'.',',');
-set(gca, 'YTickLabel', nieuw_y)
+% % Punt naar comma veranderen voor de de assen van plot 1
+% x = get(gca, 'XTickLabel');
+% nieuw_x = strrep(x(:),'.',',');
+% set(gca, 'XTickLabel', nieuw_x)
+% y = get(gca, 'YTickLabel');
+% nieuw_y = strrep(y(:),'.',',');
+% set(gca, 'YTickLabel', nieuw_y)
 
 % Label axes
 xlabel('Kracht $F$ [N]', 'Interpreter', 'latex')
-ylabel('Spanning $U$ $\times$ 10$^{-5}$ [V] ', 'Interpreter', 'latex')
+ylabel('Spanning $U$ $\times$ 10$^{-4}$ [V] ', 'Interpreter', 'latex')
 grid on
 
 
