54324b77923993e0e7c2ba86a76ce523ab9e5a3b
Brian de Keijzer <briandekeyzer@hotmail.com>
2018-01-11 17:22:15 +0100

    piezo final

--- Data/VO/frequentie metingen/resultaat.m
+++ Data/VO/frequentie metingen/resultaat.m
@@ -16,7 +16,7 @@ m_tot = massa + rust_massa;
 
 % kracht = omega.^2*r.*m_tot;
 a_z = r*omega;
-kracht = m_tot*a_z;
+kracht = m_tot*(a_z+9.81);
 
 % Hysteresis zichtbaar, dus arrays splitten voor heen en terugweg
 
@@ -39,11 +39,11 @@ gemeten_spanning_terug = gemeten_spanning(12:24);
 ft = fittype( 'poly3' );
 
 % Fit model to data.
-[fitresult1, gof] = fit( xData, yData, ft );
+[fitresult, gof] = fit( xData, yData, ft );
 
 % Plot fit with data.
+h = plot( fitresult, xData, yData, 'v' );
 hold on
-h1 = plot( fitresult1, xData, yData, 'v');
 %--------------------------------eind fit heen
 % -------------------------------Start fit terug
 %% Fit: 'untitled fit 1'.
@@ -61,12 +61,12 @@ h2 = plot( fitresult2, xData, yData, '^')
 hold on
 
 % labels & legend zelf aanpassen in createFit
-legend_fit_laaghoog = 'Fit oplopend: ax^3+bx^2+cx+d      a=5,109\times10^{-3}     b=3,887\times10^{-3}     c=-2,799\times10^{-3}     d=4,122\times10^{-4}    R^2=0,9959'
-legend_fit_hooglaag = 'Fit aflopend: ax^3+bx^2+cx+d      a=1,880\times10^{-2}     b=-7,317\times10^{-3}     c=7,181\times10^{-4}     d=2,180\times10^{-5}    R^2=0,9842'
-% legend('Meetwaarden oplopend', legend_fit_laaghoog, 'Meetwaarden aflopend',legend_fit_hooglaag, 'Interpreter', 'latex', 'Location' ,'NorthWest')
-legend('Meetwaarden oplopend','Fit oplopend','Meetwaarden aflopend', 'Fit aflopend', 'Interpreter', 'latex', 'Location' ,'NorthWest')
+legend_fit_laaghoog = 'Fit oplopend: ax^3+bx^2+cx+d      a=5,109\times10^{-3}     b=-1,493\times10^{-1}     c=1,452   d=-4,689    R^2=0,9971'
+legend_fit_hooglaag = 'Fit aflopend: ax^3+bx^2+cx+d      a=1,880\times10^{-2}     b=-5,713\times10^{-1}     c=5,786     d=-19,53   R^2=0,9790'
+legend('Meetwaarden oplopend', legend_fit_laaghoog, 'Meetwaarden aflopend',legend_fit_hooglaag, 'Interpreter', 'latex', 'Location' ,'NorthWest')
+% legend('Meetwaarden oplopend','Fit oplopend','Meetwaarden aflopend', 'Fit aflopend', 'Interpreter', 'latex', 'Location' ,'NorthWest')
 
-xlim([0 0.7])
+% xlim([0 0.7])
 % Correcte significantie maken voor plot 1
 xtickformat('%.3f')
 ytickformat('%.3f')
